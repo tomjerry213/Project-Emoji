b40523dcba12aa6a90f97d04ac97f3797e7624ea
fgtmiao <1700012824@pku.edu.com>
2020-04-30 12:47:31 +0800

    lsl added an untracked file(because of LF & CRLF)

---
+++ miniprogram/pages/emg_display/emg_display.js
@@ -0,0 +1,80 @@
+// miniprogram/pages/emg_display/emg_display.js
+Page({
+
+  /**
+   * 页面的初始数据
+   * 单个表情包的各种数据，后续与后端可以再加
+   */
+  data: {
+    /** get Image url from other pages like emg_class
+     *  or maybe load it from database
+     *  load from database please also load its features
+     * some are just for demo 
+    */
+    ImgUrl:'/emg_type/rec_0.jpg',
+    author:'匿名',
+    tags:["软工作业",'程序员','在改了','就硬拖'],
+    type:'',
+    style:''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var inpVal = options.ImgUrl
+    console.log(inpVal)
+    this.setData({
+      ImgUrl: inpVal//["detail"]["ImgUrl"]//or other
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

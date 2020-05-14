3016a695fae5e09ed26471331418a588294d681f
yellow-binary-tree <wangyueqian@pku.edu.cn>
2020-05-14 21:56:48 +0800

    添加用户信息查询云函数

---
+++ cloudfunctions/getUserInfo/index.js
@@ -0,0 +1,36 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk');
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    const wxContext = cloud.getWXContext();
+    const db = cloud.database();
+    var openid = wxContext.OPENID;
+
+    // 根据openid查询user集合中的记录。
+    var result = await db.collection('user').where({openid: openid}).get();
+    if (result.data.length != 0) result = result.data[0];
+
+    // 如果目前user集合中还没有和当前用户的openid有关的记录，则添加一条新的。
+    else {
+        console.log("adding new userInfo into database.");
+        var new_id = await db.collection('user').add({
+            data: {
+                openid: openid,
+                privilege: 2, // 默认权限是普通该用户
+                score: 0,
+                stickerList: [],
+                followedUserList: []
+            }
+        });
+        result = await db.collection('user').get(new_id);
+        result = result.data[0];
+    }
+    console.log("result:", result);
+    
+    return {
+        result: result,
+        wxContext: wxContext
+    }
+}
--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,20 +1,32 @@
 //app.js
 App({
-  onLaunch: function () {
-    
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        // env 参数说明：
-        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //   如不填则使用默认环境（第一个创建的环境）
-        env: 'local-rh75e',
-        traceUser: true,
-      })
+    onLaunch: async function () {
+        var that = this;
+        
+        if (!wx.cloud) {
+            console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+        } else {
+            wx.cloud.init({
+                // env 参数说明：
+                // env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+                // 此处请填入环境 ID, 环境 ID 可打开云控制台查看
+                // 如不填则使用默认环境（第一个创建的环境）
+                env: 'local-rh75e',
+                traceUser: true,
+            })
+        }
+        this.globalData = {}
+        
+        // 从云端获取用户的权限信息，存储在App.globalData.userInfo中
+        wx.cloud.callFunction({
+            name: 'getUserInfo', data: {},
+            success(res){
+                that.globalData.userInfo = res.result.result;
+                console.log("get userinfo cloudfunction: ", res.result.result);
+            },
+            fail(res){
+                console.log("failed to get userinfo from cloud.");
+            }
+        });
     }
-
-    this.globalData = {}
-  }
 })
